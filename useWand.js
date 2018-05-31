f91a1232e70b1e1e76c3c6136ce43cd192b0419d
Dale Evans <dale.evans.3@gmail.com>
2018-05-31 17:31:35 -0500

    V0.1
    Initial set-up, a little clunky, but everything is working at least.
    
    To-do:
    clean up dice buttons/custom dice roller
    prettify everything

---
+++ useWand.js
@@ -0,0 +1,68 @@
+	function getRandomInt(max){
+		let result = 0;
+		let min = 1;
+
+		result = Math.floor(Math.random() * (max - min + 1)) + min;
+		//alert ("Your result is: "+ result);
+		let el = document.querySelector("#diceResult");
+		el.innerHTML += 'You rolled a ' + result + '!<br />'
+	};
+	
+	function getRandomCustom(){
+		let numDice = document.getElementById('numDice').value;
+		let sizeDice = document.getElementById('sizeDice').value;
+		let result = 0;
+		let min = 1;
+		var i;
+		for (i = 0; i < numDice; i++){
+			result += Math.floor(Math.random() * (sizeDice - min + 1)) + min;
+		}
+		//alert ("Your result is: "+ result);
+		let el = document.querySelector("#diceResult");
+		el.innerHTML += 'You rolled a ' + result + '!<br />'
+	};
+
+$(function() {
+
+	$("#wandButton").click(function() {
+		try {
+			let result = 0;
+			result = Math.floor(Math.random() * (9999 - 1)) + 1;
+			let el = document.querySelector('#wandResult');
+			el.innerHTML += effectArray[result] + '<br />';
+		}
+		catch(e) {
+			alert (e || e.message);
+		}
+	});
+	
+	$("#wandReset").click(function(){
+		try {
+			let el = document.querySelector('#wandResult');
+			el.innerHTML = ' ';
+		}
+		catch(e) {
+			alert (e || e.message);
+		}
+	});
+
+	$("#diceReset").click(function(){
+		try {
+			let el = document.querySelector('#diceResult');
+			el.innerHTML = ' ';
+		}
+		catch(e) {
+			alert (e || e.message);
+		}
+	});
+
+
+
+	/*function useWand() {
+		let result = 0;
+		let min = 1;
+		result = Math.floor(Math.random() * (9999 - min + 1)) + min;
+		let el = document.querySelector('wandResults');
+		el.innerHTML +'<p> You rolled ' + result + '!</p>';
+	};*/
+});
