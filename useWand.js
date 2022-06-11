13499a1635fcca01924a091a016480958a4c1942
cudaman73 <dale.evans.3@gmail.com>
2022-06-11 17:04:55 -0500

    Refactors code for clarity and optimizes functions

--- useWand.js
+++ useWand.js
@@ -1,66 +1,40 @@
-	function getRandomInt(max){
-		let result = 0;
-		let min = 1;
-
-		result = Math.floor(Math.random() * (max - min + 1)) + min;
-		//alert ("Your result is: "+ result);
-		let el = document.querySelector("#diceResult");
-		el.innerHTML += 'You rolled a ' + result + '!<br />'
+	function getRandomInt(max) {
+	  let result = 0;
+	  result = Math.floor(Math.random() * max) + 1;
+	  $("#diceResult").append('You rolled a ' + result + '!<br />')
 	};
 
-	function getRandomCustom(){
-		let numDice = document.getElementById('numDice').value;
-		let sizeDice = document.getElementById('sizeDice').value;
-		let result = 0;
-		let min = 1;
-		var i;
-		for (i = 0; i < numDice; i++){
-			result += Math.floor(Math.random() * (sizeDice - min + 1)) + min;
-		}
-		//alert ("Your result is: "+ result);
-		let el = document.querySelector("#diceResult");
-		el.innerHTML += 'You rolled a ' + result + '!<br />'
+	function getRandomCustom() {
+	  let roll = prompt("Please enter your dice roll in the format XdXX (eg 4d6):")
+	  let numDice = roll.split('d')[0]
+	  let sizeDice = roll.split('d')[1]
+	  let total = 0;
+	  let resultArray = [];
+
+	  for (let i = 0; i < numDice; i++) {
+	    let result = Math.floor(Math.random() * sizeDice) + 1;
+			total += result;
+			resultArray.push(result)
+	  }
+	  //alert ("Your result is: "+ result);
+	  $("#diceResult").append('Rolling ' + roll + ': ' + total + '!<br />')
+		$("#diceResult").append('Individual rolls: ' + resultArray + '<br />')
 	};
 
-$(function() {
+	$(function() {
 
-	$("#wandButton").click(function() {
-		try {
-			let result = 0;
-			result = Math.floor(Math.random() * (9999 - 1)) + 1;
-			let el = document.querySelector('#wandResult');
-			el.innerHTML += effectArray[result] + '<br />';
-		}
-		catch(e) {
-			alert (e || e.message);
-		}
+	  $("#wandButton").click(function() {
+	      let result = 0;
+	      result = Math.floor(Math.random() * 9998) + 1;
+	      $('#wandResult').append(effectArray[result] + '<br />')
+	  });
+
+	  $("#wandReset").click(function() {
+	    $('#wandResult').text(' ');
+	  });
+
+	  $("#diceReset").click(function() {
+	    $('#diceResult').text(' ');
+	  });
+
 	});
-
-	$("#wandReset").click(function(){
-		try {
-			document.querySelector('#wandResult').innerHTML = ' ';
-		}
-		catch(e) {
-			alert (e || e.message);
-		}
-	});
-
-	$("#diceReset").click(function(){
-		try {
-			document.querySelector('#diceResult').innerHTML = ' ';
-		}
-		catch(e) {
-			alert (e || e.message);
-		}
-	});
-
-
-
-	/*function useWand() {
-		let result = 0;
-		let min = 1;
-		result = Math.floor(Math.random() * (9999 - min + 1)) + min;
-		let el = document.querySelector('wandResults');
-		el.innerHTML +'<p> You rolled ' + result + '!</p>';
-	};*/
-});
